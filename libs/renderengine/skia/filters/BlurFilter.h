b4b1eb1b34664f8355c43fee3e7937242abc7bc3
Danny Lin <danny@kdrag0n.dev>
2022-07-05 00:35:34 +0800

    skia: blur: Downscale source FBO to 20% size
    This is as low as we can go (in steps of 5%) without significant
    artifacts on 1440p.
    
    Change-Id: I029f0de3e94dcd8fae3ea548ab1b86e2093674b3
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>

--- libs/renderengine/skia/filters/BlurFilter.h
+++ libs/renderengine/skia/filters/BlurFilter.h
@@ -35,7 +35,7 @@ namespace skia {
 class BlurFilter {
 public:
     // Downsample FBO to improve performance
-    static constexpr float kInputScale = 0.25f;
+    static constexpr float kInputScale = 0.2f;
     // Downsample scale factor used to improve performance
     static constexpr float kInverseInputScale = 1.0f / kInputScale;
     // Maximum number of render passes
