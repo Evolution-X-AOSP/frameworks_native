4375e59209887d2a482f60b8daa81799a51f2ab6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-02 22:15:44 +0530

    SF: Defer setDesiredMode when display is off
    This is required on some devices with video mode panel, since the
    scheduler sets refresh rate to 120 Hz _after_ turning screen off at
    60 Hz (VRR idle) but the HWC ignores this and when the display is
    turned back on SF thinks we are back at 120 Hz while HWC is still
    rendering at 60 Hz. Defer setting refresh rate until the screen is
    turned back on, to avoid this confusion.
    
    To enable, set: debug.sf.defer_refresh_rate_when_off=1
    
    Change-Id: I3e531e601e9cb015b3066ae98d4d9c5436b46619

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -523,6 +523,9 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("debug.sf.dim_in_gamma_in_enhanced_screenshots", value, 0);
     mDimInGammaSpaceForEnhancedScreenshots = atoi(value);
 
+    property_get("debug.sf.defer_refresh_rate_when_off", value, "0");
+    mDeferRefreshRateWhenOff = atoi(value);
+
     mIgnoreHwcPhysicalDisplayOrientation =
             base::GetBoolProperty("debug.sf.ignore_hwc_physical_display_orientation"s, false);
 
@@ -1252,6 +1255,12 @@ void SurfaceFlinger::setDesiredMode(display::DisplayModeRequest&& request, bool 
     const auto display = getDisplayDeviceLocked(displayId);
     if (!display) {
         ALOGW("%s: display is no longer valid", __func__);
+        return;
+    }
+
+    if (mDeferRefreshRateWhenOff && display->getPowerMode() == hal::PowerMode::OFF) {
+        ALOGI("%s: deferring because display is powered off", __func__);
+        mLastActiveMode = request.mode;
         return;
     }
 
@@ -5972,6 +5981,11 @@ void SurfaceFlinger::setPowerModeInternal(const sp<DisplayDevice>& display, hal:
         }
 
         getHwComposer().setPowerMode(displayId, mode);
+        if (mLastActiveMode) {
+            ALOGI("Deferred active mode change pending, applying now");
+            setDesiredMode({mLastActiveMode.value()}, true);
+            mLastActiveMode = std::nullopt;
+        }
         if (mode != hal::PowerMode::DOZE_SUSPEND &&
             (displayId == mActiveDisplayId || FlagManager::getInstance().multithreaded_present())) {
             const bool enable =
